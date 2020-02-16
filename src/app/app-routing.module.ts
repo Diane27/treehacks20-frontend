7d9b94d68ec0ebb1b3d07bac758861d6851340ad
Priya Talreja <priyatalreja@Priyas-MacBook-Air.local>
2020-02-15 20:19:21 -0800

    added onboarding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'onboarding',
+    loadChildren: () => import('./onboarding/onboarding.module').then( m => m.OnboardingPageModule)
   }
 ];
 @NgModule({
